1881e68b4beab343e4652a39bc18a9614363a0b6
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-07-06 13:16:23 +0300

    Some code
    --Now we printing the header of
      incoming request to the stdout
    
    Note:
    Finally something that works

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
 pub use server::HttpServer;
 
 mod server;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,27 @@
+use buffer::ReadBuffer;
 use httparse::Result;
 use threads_pool::ThreadPool;
 use std::collections::HashMap;
+use std::net::{TcpListener, TcpStream};
 
+type Pages = HashMap<String, Box<dyn Fn() -> String>>;
+
+///# HttpServer struct.
+///
+///This is the actual http server.
+///Just instantiate and add pages and resources
+///
+///# Example
+///```
+///let mut server = weber::HttpServer::new();
+///
+///server.add_page("/".to_string(), || {
+///   "some_page".to_string()
+///});
+///```
 pub struct HttpServer {
     workers: ThreadPool,
-    pages: HashMap<String, Box<dyn Fn() -> String>>
+    pages: Pages
 }
 
 impl HttpServer {
@@ -27,6 +44,25 @@ impl HttpServer {
     }
 
     pub fn run(&mut self) {
+        let listener = TcpListener::bind("127.0.0.1:7080")
+            .unwrap();
 
+        for connection in listener.incoming() {
+            self.workers.execute(|| {
+                match connection {
+                    Ok(stream) => Self::handle_connection(stream),
+                    Err(_) => {}
+                }
+            });
+        }
+    }
+
+    fn handle_connection(mut stream: TcpStream) {
+        let mut header = Vec::with_capacity(512);
+        stream.read_buffer(&mut header).unwrap();
+
+        let header = String::from_utf8_lossy(&header);
+
+        println!("header: {}", header);
     }
 }
