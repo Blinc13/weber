5cf05db69c1788a233a56f0c081fcb7eb8fc4bc3
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-07-11 00:47:10 +0300

    Adding Tests
    --Added tests for HttpData

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-extern crate core;
-
 pub use server::HttpServer;
 
 pub mod parser;
 pub mod server;
-mod net;
+pub mod net;
---
+++ tests/httpdata_test.rs
@@ -0,0 +1,26 @@
+use weber::{
+    net::HttpData,
+    parser::{
+        Builder,
+        response::builder::ResponseBuilder
+    }
+};
+
+#[test]
+fn httpdata_test() {
+    let resp = ResponseBuilder::new().build();
+
+    let parsed = HttpData::parse(resp.as_bytes()).unwrap();
+    let parsed = parsed.as_response();
+
+    assert_eq!(parsed.status_code, 200);
+}
+
+#[test]
+#[should_panic]
+fn httpdata_panic_test() {
+    let resp = ResponseBuilder::new().build();
+
+    let parsed = HttpData::parse(resp.as_bytes()).unwrap();
+    let parsed = parsed.as_request();
+}
