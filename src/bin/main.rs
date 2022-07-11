cdc8c0626f607b52edc2082fbd51b9172c34af58
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-07-11 16:58:08 +0300

    New example

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,40 @@
+use std::collections::HashMap;
 use weber::HttpServer;
 
 fn main() {
     let mut server = HttpServer::new(4);
 
-    server.add_page("/".to_string(), | _ | {
-        return "<!DOCTYPE html>
+    let mut default = HashMap::new();
+
+    default.insert("foo".to_string(), "gg".to_string());
+    default.insert("bar".to_string(), "hi".to_string());
+
+    server.add_page("/".to_string(),move | request | {
+        let vals = request.path.values.as_ref().unwrap_or(&default);
+
+        let foo = match vals.get("foo") {
+            None => "Variable not defined",
+            Some(foo) => foo
+        };
+
+        let bar = match vals.get("bar") {
+            None => "Variable not defined",
+            Some(bar) => bar
+        };
+
+        format!("<!DOCTYPE html>
         <html>
             <head>
                 <h1>Hello world!</h1>
             </head>
+            <body>
+                <h3>Foo: {}</h3>
+                <h3>Bar: {}</h3>
+
+                <a href=\"/:foo=bar,bar=foo\">Some page</a>
+            </body>
         </html>
-        "
-        .to_string();
+        ", foo, bar)
     });
 
     server.run();
