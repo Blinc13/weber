8e48a67ef0691d428b64c5bbd173e52a5aafa114
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-07-13 03:28:21 +0300

    Adding Documentation

--- src/parser/content.rs
+++ src/parser/content.rs
@@ -1,6 +1,9 @@
 use std::fmt::{Formatter, Display};
 use ContentType::*;
 
+///Structure for the content returned by the page
+///
+///From the example in the readme, you can already understand how to use
 pub struct Content {
     pub content: Vec<u8>,
     pub r#type: ContentType
@@ -15,6 +18,9 @@ impl Content {
     }
 }
 
+///Enumeration of content-type header values
+///
+///Just return what the content is
 #[derive(Debug)]
 pub enum ContentType {
     Html,
